5084abe1d8ba4bc0844ceab07aec2c6b59583c10
VSoups <vince.kubota@gmail.com>
2024-04-16 13:18:32 -0700

    Date error correction

--- models/flight.js
+++ models/flight.js
@@ -8,7 +8,7 @@ const flightSchema = new Schema({
     airport: { type: String, enum: ['LAX', 'SFO', 'ATL', 'DBX', 'LHR', 'HND'], default: 'SFO' },
     flightNum: { type: Number, min: 10, max: 9999, required: true },
     departs: { type: Date, default: function() { // function to set default time
-        const today = new Date().toLocaleString(); // save full date
+        const today = new Date(); // save full date
         const year = today.getFullYear(); // save only year from the previous line
         return today.setFullYear(year + 1); // return the full date we saved but add 1 to the year (as default if nothing is chosen by the user)
     } },
